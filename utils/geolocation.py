0c97b575efbed8843e7c84f19f8c04aea25d0008
Omkar Mandhare <omkarmandhare26@gmail.com>
2025-12-14 16:30:21 +0530

    feat: data cleaning and data transformation on columns - violation type, charge, article, contributed to accident, race, gender, driver state, DL state, arrest type, geolocation

---
+++ utils/geolocation.py
@@ -0,0 +1,26 @@
+from .coordinates import clean_latitude, clean_longitude
+
+def format_and_clean_geolocation(val):
+    try:
+        # Getting values of latitude and longitude from tuple as a string
+        lat, lon = str(val).strip("()").split(",")
+        geo_lat = clean_latitude(round(float(lat), 6))
+        geo_lon = clean_longitude(round(float(lon), 6))
+
+
+        # Removing invalid data
+        if geo_lat == None or geo_lon == None: return None
+    except Exception as e:
+        return None
+    return (geo_lat, geo_lon)
+
+def validate_geolocation(row): 
+    geo_lat, geo_lon = row["Geolocation"]
+    latitude = row["Latitude"]
+    longitude = row["Longitude"]
+
+    # Fixing Geolocation values if they differ from column Latitude and Longitude
+    if latitude != geo_lat: geo_lat = latitude
+    if longitude != geo_lon: geo_lon = longitude
+
+    return (geo_lat, geo_lon)
