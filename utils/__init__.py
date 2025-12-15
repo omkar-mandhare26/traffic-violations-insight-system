7516ebdbe2ca4005836da5d6050a8ea6771a3d8d
Omkar Mandhare <omkarmandhare26@gmail.com>
2025-12-15 13:41:10 +0530

    feat: data cleaning and data transformation on Model column

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,4 +5,5 @@ from .driver_city import invalid_driver_city, prefix_mapping
 from .geolocation import format_and_clean_geolocation, validate_geolocation
 from .location import clean_location
 from .make import invalid_make
+from .model import invalid_model
 from .time_of_stop import clean_time_of_stop
